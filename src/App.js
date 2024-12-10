0ba675d72764e8277530b5e7b238fd5fd54e408b
charlybgood__x <bonavita.carlos@gmail.com>
2024-12-10 17:13:34 -0300

    modify path when user enters url and sesion is active

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { Footer } from "./components/Footer";
 import { Home } from "./components/Home";
 import { Login } from "./components/Login";
@@ -7,29 +7,36 @@ import { ProtectedRoute } from "./components/ProtectedRoute";
 import { Register } from "./components/Register";
 import { ResetPassword } from "./components/ResetPassword";
 import { Welcome } from "./components/Welcome";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider, useAuth } from "./context/AuthContext";
 
 function App() {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  // Redirigir si el usuario está autenticado
+  if (user) {
+    navigate("/Home");
+  }
   return (
     <div className="todo-app">
       <AuthProvider>
         <Navbar />
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                  <Welcome />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/Home" element={<Home />} />
-            <Route path="/Welcome" element={<Welcome />} />
-            <Route path="/Login" element={<Login />} />
-            <Route path="/Register" element={<Register />} />
-            <Route path="/ResetPassword" element={<ResetPassword />} />
-          </Routes>
-          <Footer />        
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Welcome />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Welcome" element={<Welcome />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/ResetPassword" element={<ResetPassword />} />
+        </Routes>
+        <Footer />
       </AuthProvider>
     </div>
   );
