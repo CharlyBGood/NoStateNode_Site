df53b40b4b083347e348f2189bbf5f2534db1dc5
charlybgood__x <bonavita.carlos@gmail.com>
2024-12-12 19:03:39 -0300

    add detection to renderize text or link based on user input

--- src/formPages/Task.js
+++ src/formPages/Task.js
@@ -1,11 +1,31 @@
 import "../stylesheets/Task.css";
 import { FaTrash } from "react-icons/fa";
 
+const isValidUrl = (string) => {
+  try {
+    new URL(string);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
 function Task({ id, text, complete, completeTask, deleteTask }) {
+  const renderContent = () => {
+    if (isValidUrl(text)) {
+      return (
+        <a href={text} target="_blank" rel="noopener noreferrer" className="todo-link">
+          {text}
+        </a>
+      );
+    }
+    return <span>{text}</span>
+  }
+
   return (
     <div className={complete ? "todo-container complete" : "todo-container"}>
       <div className="todo-txt" onClick={() => completeTask(id)}>
-        {text}
+        {renderContent()}
       </div>
       <div className="todo-container-icons" onClick={() => deleteTask(id)}>
         <FaTrash className="todo-icon" />
